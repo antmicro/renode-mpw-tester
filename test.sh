bf2b9ae4a1549cbe47ff9c3d86cc26e3f63e5902
Łukasz Mędrek <lmedrek@internships.antmicro.com>
2022-09-22 15:08:46 +0200

    [#37542] Add script for testing

---
+++ test.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+set -e
+
+GITHUB_WORKSPACE=${GITHUB_WORKSPACE:-$(pwd)}
+
+RENODE_VERSION_DEFAULT=1.13.1+20220918git57f09419
+RENODE_VERSION=${RENODE_VERSION:-$RENODE_VERSION_DEFAULT}
+RENODE_DIR_DEFAULT="."
+RENODE_DIR=${RENODE_DIR:-$RENODE_DIR_DEFAULT}
+
+usage()
+{
+    echo "$0 [-v VERSION ] [-d DIRECTORY ] [-T]"
+    echo ""
+    echo " -v VERSION - Set Renode version to use, default is $RENODE_VERSION_DEFAULT"
+    echo " -d DIRECTORY - Set Renode directory to use, default is $RENODE_DIR_DEFAULT"
+}
+
+run_test()
+{
+    renode-run -a "$RENODE_DIR" download -d "$RENODE_VERSION"
+    renode-run -a "$RENODE_DIR" test -- -r "$GITHUB_WORKSPACE"/artifacts "$GITHUB_WORKSPACE"/artifacts/*.robot
+}
+
+while getopts "v:d:" option; do
+    case "$option" in
+        v) RENODE_VERSION="$OPTARG";;
+        d) RENODE_DIR="$OPTARG";;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+run_test
